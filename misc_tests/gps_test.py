d024dd11f1a256c4026dd5e86b9b7525fbe1f6ca
Eric Hardy <eric.hardy@univ-brest.fr>
2020-01-22 12:08:32 +0100

    get data

--- misc_tests/gps_test.py
+++ misc_tests/gps_test.py
@@ -9,6 +9,38 @@ from pygeodesy.utily import radians
 
 from pdb import set_trace as st
 
+gpsKeys = []
+gpsKeys.append('lat')
+gpsKeys.append('lon')
+gpsKeys.append('track')
+gpsKeys.append('time')
+gpsKeys.append('speed')
+gpsKeys.append('ept')
+gpsKeys.append('alt')
+gpsKeys.append('mode')
+gpsKeys.append('epv')
+
+gpsData = []
+
+# ggppss = os.open('ggppssX', os.O_CREAT|os.O_APPEND)
+ggppss = open('ggppssX', 'a')
+
+def getData(report):
+    data = {}
+    for d in gpsKeys:
+        try:
+            data[d] = report[d]
+        except:
+            data[d] = 0.0
+    return data
+
+def gpsSTR(data):
+    gps_s = ''
+    for d in gpsKeys:
+        gps_s += str(data[d]) + '|'
+    gps_s += '\n'
+    return gps_s
+
 session = gps(mode=WATCH_ENABLE)
 try:
     while True:
@@ -26,15 +58,26 @@ try:
             session = gps(mode=WATCH_ENABLE)
         # st()
         # print("%f %f" % report['lat'], report['lon'])
-        # print(report)
         if report['class'] == 'TPV':
+            data = getData(report)
+            ggppss.write(gpsSTR(data))
+            print(gpsSTR(data))
             # print(report)
-            print("%f %f %f %s %f" % (
+            # print(report)
+            """
+            print("%f %f %f %s %f %f %f %i %f" % (
                 report['lat'],
                 report['lon'],
                 report['track'],
                 report['time'],
-                report['speed']))
+                report['speed'],
+                report['ept'],
+                report['alt'],
+                report['mode'],
+                report['epv']
+                ))
+           """
+
     # Do more stuff
 except StopIteration:
     print ("GPSD has terminated")
